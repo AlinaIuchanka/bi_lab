6a08feb62d2eec2fdc5e6b6294f13ce94ab9b763
AlinaIuchanka <37531179+AlinaIuchanka@users.noreply.github.com>
2018-05-30 23:31:15 +0300

    Fix whitespaces task03_1

--- task03/task03_1.py
+++ task03/task03_1.py
@@ -10,7 +10,7 @@ print(list2)
 
 # Use a list comprehension to construct
 # the list ['1a', '2a', '3a', '4a'].
-list3 = [str(i) + 'a' for i in range(1, 5)] 
+list3 = [str(i) + 'a' for i in range(1, 5)]
 print(list3)
 
 # Simultaneously remove the element '2a'
