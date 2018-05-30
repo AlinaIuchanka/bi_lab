24e307054f662aab798d97f710f191f68fe5fcea
AlinaIuchanka <37531179+AlinaIuchanka@users.noreply.github.com>
2018-05-30 23:22:47 +0300

    Update task03_1

--- task03/task03_1.py
+++ task03/task03_1.py
@@ -1,6 +1,6 @@
 # Use a list comprehension to construct
 # the list ['ab', 'ac', 'ad', 'bb', 'bc', 'bd'].
-list1 = ['ab', 'ac', 'ad', 'bb', 'bc', 'bd']
+list1 = [i + j for i in 'ab' for j in 'bcd']
 print(list1)
 
 # Use a slice on the above list to construct
@@ -10,7 +10,7 @@ print(list2)
 
 # Use a list comprehension to construct
 # the list ['1a', '2a', '3a', '4a'].
-list3 = ['1a', '2a', '3a', '4a']
+list3 = [str(i) + 'a' for i in range(1, 5)] 
 print(list3)
 
 # Simultaneously remove the element '2a'
